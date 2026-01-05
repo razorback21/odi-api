8ea9b2b3d663b286c6174cc6dc5831c26ababb2c
donz <donnygapulao@gmail.com>
2026-01-06 06:53:12 +0800

    disabled webroutes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,24 @@
 <?php
 
 use App\Http\Controllers\ContactController;
-use Illuminate\Http\Request;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/login', function (Request $request) {
+    return htmlspecialchars("Goto '/api/generate-token' to generate Sanctum access token. Then use it in the Authorization header as 'Bearer <token>'.");
+})->name('login');
+
+Route::get('/generate-token', function () {
+    $user = User::createOrFirst([
+        'name' => 'Test User',
+        'email' => 'user@example.com',
+    ], [
+        'password' => bcrypt('password'),
+    ]);
+
+    $token = $user->createToken('super-token')->plainTextToken;
+
+    return $token;
+});
 
 Route::apiResource('contacts', ContactController::class)->middleware('auth:sanctum');
