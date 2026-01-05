18d2837018773b70d23e987b33a59061c0e9fb5f
donz <donnygapulao@gmail.com>
2026-01-06 05:42:32 +0800

    remove swagger annotations

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,17 +7,11 @@ use App\Models\Contact;
 
 class ContactController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return Contact::all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(ContactStoreRequest $request)
     {
         $contact = Contact::create($request->validated());
@@ -25,17 +19,11 @@ class ContactController extends Controller
         return response()->json($contact, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Contact $contact)
     {
         return response()->json($contact);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(ContactStoreRequest $request, Contact $contact)
     {
         $contact->update($request->validated());
@@ -43,9 +31,6 @@ class ContactController extends Controller
         return response()->json($contact);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Contact $contact)
     {
         $contact->delete();
