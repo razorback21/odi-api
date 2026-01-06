4d1d54a7e63decb87596c74e07a18e5db265d69e
donz <donnygapulao@gmail.com>
2026-01-06 14:30:33 +0800

    migration

---
+++ database/migrations/2026_01_05_140413_create_students_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id('student_id');
+            $table->string('student_code')->nullable();
+            $table->string('first_name');
+            $table->string('date_of_birth')->nullable();
+            $table->string('school_code')->index()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
