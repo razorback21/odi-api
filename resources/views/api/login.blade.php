22b2e892c16e00dce5e70632b923a62e8dc8ee63
donz <donnygapulao@gmail.com>
2026-01-06 11:50:44 +0800

    format code

--- resources/views/api/login.blade.php
+++ resources/views/api/login.blade.php
@@ -2,6 +2,7 @@
   <div class="container">
     <div class="flex justify-center items-center h-screen w-full">
       <div class="w-full max-w-md">
+
         <h1 class="text-center text-2xl font-bold mb-6">This is an API only endpoint</h1>
         <div class="p-4 rounded-lg bg-gray-800 w-lg">
           <p class="mb-4 text-gray-300">
@@ -11,7 +12,6 @@
             token. {{ 'Then use it in the Authorization header as \'Bearer <token>\'.' }}
           </p>
         </div>
-
 
       </div>
     </div>
